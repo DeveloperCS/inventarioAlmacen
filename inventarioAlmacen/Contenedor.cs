b4498a9833bb309f29640aba1404f00fd7df5cd8
karlosSeyber <42704443+karlosSeyber@users.noreply.github.com>
2018-11-28 01:42:00 -0600

    Esquinas redondas

--- inventarioAlmacen/Contenedor.cs
+++ inventarioAlmacen/Contenedor.cs
@@ -15,7 +15,21 @@ namespace inventarioAlmacen
         public Contenedor()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
         }
+
+        //Redondear esquinas
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+        int nLeftRect, // x-coordinate of upper-left corner
+        int nTopRect, // y-coordinate of upper-left corner
+        int nRightRect, // x-coordinate of lower-right corner
+        int nBottomRect, // y-coordinate of lower-right corner
+        int nWidthEllipse, // height of ellipse
+        int nHeightEllipse // width of ellipse
+        );
 
         //Arrastrar Formulario
         [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
