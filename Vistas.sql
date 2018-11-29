03beebface9214c40ba4dcd142e615a259e98405
Irene Margarita <irene.delacruz@sistemas.tecsanpedro.edu.mx>
2018-11-29 12:08:31 -0600

    Create Vistas.sql

---
+++ Vistas.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW ConsumoE
+ AS SELECT IdUsuario,IdEmpleado,NombreArticulo, FechaSalida
+ FROM Electronico
+
+ CREATE VIEW PrestadosR
+ AS SELECT IdUsuario,IdEmpleado,NombreArticulo,FechaSalida, FechaEntrega
+ FROM Recibos
+
+ CREATE VIEW SinArticulos
+ AS SELECT IdArticulo, NombreArticulo, categoria
+ FROM Articulos
+ WHERE CantidadAlmacen = 0
