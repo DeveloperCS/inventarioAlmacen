28662dc3cba339ba391a57437000ded62446522c
Irene Margarita <irene.delacruz@sistemas.tecsanpedro.edu.mx>
2018-11-29 12:53:41 -0600

    VISTA

---
+++ VISTAS2.sql
@@ -0,0 +1,5 @@
+﻿  CREATE VIEW ArticulosCategoriaHL
+  AS SELECT * FROM Articulos WHERE Categoria= 'Higiene y Limpieza'
+
+  CREATE VIEW ArticulosCategoriaHO
+  AS SELECT* FROM Articulo where Categoria='Herramientas y Otros'
