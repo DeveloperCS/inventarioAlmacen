3d0bb67e229a659c53e1e8523cf65e0f811e9223
Irene Margarita <irene.delacruz@sistemas.tecsanpedro.edu.mx>
2018-11-29 12:44:55 -0600

    Vistas2

--- Vistas.sql
+++ Vistas.sql
@@ -10,3 +10,9 @@
  AS SELECT IdArticulo, NombreArticulo, categoria
  FROM Articulos
  WHERE CantidadAlmacen = 0
+
+  CREATE VIEW ArticulosCategoriaHL
+  AS SELECT * FROM Articulos WHERE Categoria= 'Higiene y Limpieza'
+
+  CREATE VIEW ArticulosCategoriaHO
+  AS SELECT* FROM Articulo where Categoria='Herramientas y Otros'
