fcd5bec70b5a5dfe155d7c2afd97b9b7564df651
Irene Margarita <irene.delacruz@sistemas.tecsanpedro.edu.mx>
2018-12-12 11:53:52 -0600

    ddd

--- MODIFICACION2.sql
+++ MODIFICACION2.sql
@@ -4,3 +4,12 @@ ADD FOREIGN KEY (IdArticulo) REFERENCES Articulos(IdArticulo);
 
 ALTER TABLE Recibos
 ADD FOREIGN KEY (IdArticulo) REFERENCES Articulos(IdArticulo);
+
+Create trigger actualizarArticulos
+on Articulos
+for insert, update
+as 
+select * from MostrarUsuario
+go
+
+select* from recibos
