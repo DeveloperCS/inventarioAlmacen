e1e831d95576fe71e04dd301e1f7f99b93e11121
Irene Margarita <irene.delacruz@sistemas.tecsanpedro.edu.mx>
2018-12-15 17:46:26 -0600

    ,,,

--- ProcedimientoInsert.sql
+++ ProcedimientoInsert.sql
@@ -5,9 +5,7 @@
 as 
 begin
 insert into Recibos values (@IdRecibo, @IdUsuario, 
-@IdArticulo, @NombreArticulo,
-@FechaSalida, @FechaEntrega, @Descripcion,
-@IdEmpleado)
+@IdArticulo, @NombreArticulo,@FechaSalida, @FechaEntrega, @Descripcion,@IdEmpleado)
 end
 go
 
@@ -20,3 +18,34 @@ begin
 insert into Electronico values (@IdElectronico, @IdUsuario, @IdArticulo, @idEmpleado, @NombreArticulo, @CantidadArticulo, @FechaSalida)
 end
 go
+
+
+CREATE PROC InsertArticulos
+@IdArticulo varchar (7), @NombreArticulo nvarchar (250),@Categoria nVarchar (25), @CantidadInicial float,
+@CantidadAlmacen float, @TipoMedida nVarchar(6), @Estado nVarchar (25)
+as 
+begin
+insert into Articulos values (@IdArticulo, @NombreArticulo,@Categoria, @CantidadInicial, @CantidadAlmacen, @TipoMedida, @Estado)
+end
+go
+
+
+CREATE VIEW InsertarEmpleados
+  @IdEmpleado nVarchar(7),@NombreEmpleado nvarchar(20), @ApellidoPaterno nVarchar(20),@ApellidosMaterno nVarchar(20), @ClaveEmpleado nVarchar(4),@Puesto nVarchar(50) 
+as 
+begin
+insert into Empleados values (@IdEmpleado,@NombreEmpleado, @ApellidoPaterno,@ApellidosMaterno, @ClaveEmpleado,@Puesto)
+end
+go
+
+
+CREATE VIEW InsertarUsuario
+  @IdUsuario nVarchar(7),@NombreUsuario nvarchar(30), @ApellidoPaterno nVarchar(20),@ApellidosMaterno nVarchar(20), @Usuario nVarchar(50),@TipoUsuario nVarchar(50), @Contrseña nVarchar(15)
+as 
+begin
+insert into Usuario values (@IdUsuario,@NombreUsuario, @ApellidoPaterno,@ApellidosMaterno, @Usuario,@TipoUsuario, @Contrseña)
+end
+go
+
+
+
