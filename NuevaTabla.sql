7a2650a4acc18f0420c536bfba4b6dd0949b683d
Irene Margarita <irene.delacruz@sistemas.tecsanpedro.edu.mx>
2018-12-16 17:04:09 -0600

    modificacin de la vista

--- NuevaTabla.sql
+++ NuevaTabla.sql
@@ -22,7 +22,7 @@ CREATE VIEW ConsumoE
  FROM Recibos
 
 CREATE VIEW MostrarDevolucion
-AS SELECT IdDevolucion AS [Clave Devolucion],IdRecibo AS [CLave Recibo],IdUsuario AS [Clave Usuario],IdEmpleado AS [CLave Empleado], NombreArticulo AS [Articulo],CantidadArticulo AS [Cantidad],FechaSalida AS [Fecha Salida], 
+AS SELECT IdDevolucion AS [Clave Devolucion],IdRecibo AS [CLave Recibo],IdUsuario AS [Clave Usuario],IdEmpleado AS [CLave Empleado],IdArticulo AS [Folio], NombreArticulo AS [Articulo],CantidadArticulo AS [Cantidad],FechaSalida AS [Fecha Salida], 
  FechaEntrega AS [Fecha Entrada], Descripcion AS [Descripcion]
  FROM Devolucion
 
--- ProcedimientoDevolucion.sql
+++ ProcedimientoDevolucion.sql
@@ -7,3 +7,8 @@ convert(Varchar(7), RIGHT(MAX(IdDevolucion),4)+1),4) from Devolucion
 end
 
 exec AGIDD
+select * from MostrarDevolucion
+CREATE VIEW MostrarDevolucion
+AS SELECT IdDevolucion AS [Clave Devolucion],IdRecibo AS [CLave Recibo],IdUsuario AS [Clave Usuario],IdEmpleado AS [CLave Empleado],IdArticulo AS [Folio], NombreArticulo AS [Articulo],CantidadArticulo AS [Cantidad],FechaSalida AS [Fecha Salida], 
+ FechaEntrega AS [Fecha Entrada], Descripcion AS [Descripcion]
+ FROM Devolucion
