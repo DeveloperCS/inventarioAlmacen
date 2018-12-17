28426da4546998b0fa09c5963d3a75f8f42bdd61
Irene Margarita <irene.delacruz@sistemas.tecsanpedro.edu.mx>
2018-12-16 21:10:31 -0600

    contador

--- ProcedimientoRecibos.sql
+++ ProcedimientoRecibos.sql
@@ -6,3 +6,9 @@ LEFT(MAX(IdRecibo),6+1),
 RIGHT('/'+
 convert(Varchar(11), RIGHT(MAX(IdRecibo),4)+1),4) from Recibos
 END
+
+CREATE PROC Contador
+as
+begin
+select count (IdRecibo) from Recibos
+end
